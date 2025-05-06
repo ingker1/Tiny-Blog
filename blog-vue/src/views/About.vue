cdb3ba37e16681588eb604965cbff024f8c04d7c
ingker <haowao@outlook.com>
2025-05-06 17:00:14 +0800

    为关于和导航页面添加内容

--- blog-vue/src/views/About.vue
+++ blog-vue/src/views/About.vue
@@ -1,8 +1,38 @@
 <template>
-    <!-- 空模板 -->
-    <p></p>
-  </template>
+    <Header/>
+    <div class="container">
+        <div class="post">
+            <h1>关于</h1>
+            <p style="text-align: center; height: 40vh; font-size: 18px;"> 
+            我是清科谷体，简称清科，英文网名是ingker。<br><br>
+
+            一个使用SpringBoot和Vue.js驱动的博客网站。<br><br>
+
+            这个网站会发布自己的想法、思考和学习心得；也出一些简明的教程给需要的人看。<br><br>
+            </p>
+        </div>
+    </div>
+    <Footer/>
+</template>
   
-  <script>
-  </script>
+<script setup>
+    import Header from '@/components/Header.vue';
+    import Footer from '@/components/Footer.vue';
+</script>
+
+<style scoped>
+.container {
+    position: relative;
+    transform: translateX(-50%);
+    left: 50%;
+    width: 70vw;
+    padding: 20px;
+    margin: -8px;
+}
+
+h1 {
+    text-align: center;
+}
+
+</style>
   
--- blog-vue/src/views/Index.vue
+++ blog-vue/src/views/Index.vue
@@ -1,8 +1,36 @@
 <template>
-    <!-- 空模板 -->
-    <p></p>
-  </template>
-  
-  <script>
-  </script>
-  
+    <Header/>
+    <div class="container">
+        <h3>请选择你要访问的站点：</h3>
+        <h3><a href="https://blog.ingke.net/">访问清科谷体的博客</a></h3>
+        <h3><a href="https://rss.ingke.net/">反向代理动画下载站点rss解析</a></h3>
+        <h3><a href="https://rsshub.ingke.net/">RSSHub服务生成网站的RSS订阅</a></h3>
+    </div>
+</template>
+
+<script setup>
+    import Header from '@/components/Header.vue';
+</script>
+
+<style scoped>
+.container {
+    width: 60%;
+    margin: 10% auto 0;
+    background-color: #f0f0f0;
+    padding: 2% 5%;
+    border-radius: 10px
+}
+
+ul {
+    padding-left: 20px;
+}
+
+ul li {
+    line-height: 2.3
+}
+
+a {
+    color: #20a53a
+}
+
+</style>
